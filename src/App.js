c60d896cd93705836b9de96d306ef76383b50fef
denysfmalta <denysfmalta@hotmail.com>
2023-09-20 12:13:54 -0300

    app finished

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import { useState, useEffect } from "react";
 import './style.css';
 
 function App() {
-  const [pokemon, setPokemon] = useState({}); // Initialize pokemon state as an object
-  let searchPokemon = document.getElementByID('searchPokemon').value;
+  const [pokemonName, setPokemonName] = useState(""); // Variável para armazenar o nome do Pokémon
+  const [pokemon, setPokemon] = useState({}); // Variável para armazenar os dados do Pokémon
+
+  useEffect(() => {
+    if (pokemonName) {
+      loadAPI();
+    }
+  }, [pokemonName]);
 
   function loadAPI() {
-  let url = "https://pokeapi.co/api/v2/";
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
     fetch(url)
       .then(response => response.json())
-      .then(res => {
-        console.log(res);
-        setPokemon(res);
+      .then(data => {
+        console.log(data);
+        setPokemon(data);
       })
       .catch(err => console.log(err));
   }
-
 
   return (
     <div className='container'>
@@ -24,15 +30,25 @@ function App() {
       </header>
 
       <h2>Bem vindo à Pokédex FATEC!</h2>
-      <form onSubmit={loadAPI()}>
-      <label>Digite o nome do Pokémon:<input></input></label>
+      <form onSubmit={(e) => {
+        e.preventDefault(); // Impedir o envio do formulário padrão
+        loadAPI();
+      }}>
+        <label>Digite o nome do Pokémon:
+          <input
+            type="text"
+            onChange={(e) => setPokemonName(e.target.value)}
+            value={pokemonName}
+          />
+        </label>
+        <button type="submit">Pesquisar</button>
       </form>
 
       <div>
-        <img src={pokemon.sprites?.front_default} alt={pokemon.name} /> 
+        <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
         <div>Name: {pokemon.name}</div>
         <div>N.º {pokemon.id}</div>
-        <div>Peso: {pokemon.weight / 10}kg</div> 
+        <div>Peso: {pokemon.weight / 10}kg</div>
         <div>Altura: {pokemon.height / 10}m</div>
       </div>
     </div>
